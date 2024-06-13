047abac4d0051df41b3dce9a524339b2bb1580a4
davidjonathan01 <davidfernandezdavila01@gmail.com>
2024-06-13 01:08:56 -0500

    registrar estudiante

--- src/app/registrar-estudiante/registrar-estudiante.component.ts
+++ src/app/registrar-estudiante/registrar-estudiante.component.ts
@@ -1,33 +1,43 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
 import { Estudiante } from '../model/estudiante';
 import { EstudianteService } from '../service/estudiante.service';
 import Swal from 'sweetalert2';
-import { CommonModule } from '@angular/common';
+import { CommonModule, formatDate } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-registrar-estudiante',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule,NgxPaginationModule],
   templateUrl: './registrar-estudiante.component.html',
   styleUrl: './registrar-estudiante.component.css'
 })
 export class RegistrarEstudianteComponent {
   estudianteArray: Estudiante[] = [];
   estudianteForm: FormGroup;
+  offset: number;
+  isEdited:boolean=false;
+  page:number;
 
-  constructor(private estudianteService: EstudianteService) {
+  constructor(
+    @Inject(LOCALE_ID) public locale: string,
+    private estudianteService: EstudianteService) {
+    this.page = 1;
+    this.offset = new Date().getTimezoneOffset();
     this.estudianteForm = new FormGroup({
-      doc_identidad: new FormControl('', []),
-      nombres: new FormControl('', []),
-      apellidos: new FormControl('', []),
-      fec_nacimiento: new FormControl('', []),
-      id_genero: new FormControl('', []),
-      email: new FormControl('', []),
-      direccion: new FormControl('', []),
-      num_telefono: new FormControl('', []),
-      anio_ingreso: new FormControl('', []),
-      id_carrera: new FormControl('', [])
+      id_estudiante: new FormControl('', []),
+      doc_identidad: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]),
+      nombres: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      apellidos: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      fec_nacimiento: new FormControl('', [Validators.required]),
+      id_genero: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      direccion: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      num_telefono: new FormControl('', [Validators.required, Validators.minLength(7), Validators.maxLength(15)]),
+      anio_ingreso: new FormControl('', [Validators.required, Validators.min(1900), Validators.max(new Date().getFullYear())]),
+      id_carrera: new FormControl('', [Validators.required]),
+      contrasenia: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
   }
 
@@ -54,29 +64,129 @@ export class RegistrarEstudianteComponent {
   registrarEstudiante(): void {
     console.log('test');
     console.log(this.estudianteForm.value);
-    this.estudianteService.registrarEstudiante(this.estudianteForm.value).subscribe(
+    if (this.isEdited) {
+      this.actualizarEstudiante();
+    } else {
+      this.estudianteService.registrarEstudiante(this.estudianteForm.value).subscribe(
+        (result: any) => {
+          console.log(this.estudianteForm.value);
+          Swal.close();
+          Swal.fire({
+            icon: 'success',
+            title: 'registrarEstudiante ...',
+            text: 'Se registró exitosamente los datos del estudiante!',
+          });
+          this.estudianteForm.reset();
+          this.getEstudiantes();
+        },
+        (err: any) => {
+          console.log(this.estudianteForm.value);
+          console.log(err);
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: 'Advertencia ...',
+            text: 'Ah ocurrido un error!',
+          });
+        }
+      );
+  }
+}
+
+  editarEstudiante(estudiante: Estudiante): void {
+    Swal.fire({
+      title: '¿Está seguro de editar este estudiante?',
+      showCancelButton: true,
+      cancelButtonText: 'No',
+      confirmButtonText: 'Sí',
+      focusCancel: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.estudianteForm.setValue({
+          id_estudiante: estudiante.id_estudiante,
+          doc_identidad: estudiante.doc_identidad,
+          nombres: estudiante.nombres,
+          apellidos: estudiante.apellidos,
+          fec_nacimiento: formatDate(estudiante.fec_nacimiento, 'yyyy-MM-dd', this.locale, 'UTC' + this.offset),
+          id_genero: estudiante.id_genero,
+          email: estudiante.email,
+          direccion: estudiante.direccion,
+          num_telefono: estudiante.num_telefono,
+          anio_ingreso: estudiante.anio_ingreso,
+          id_carrera: estudiante.id_carrera,
+          contrasenia: estudiante.contrasenia
+        });
+        this.isEdited = true;
+      }
+    });
+  }
+
+  actualizarEstudiante(): void {
+    const id = this.estudianteForm.value.id_estudiante;
+
+    this.estudianteService.actualizarEstudiante(id, this.estudianteForm.value).subscribe(
       (result: any) => {
-        console.log(this.estudianteForm.value);
         Swal.close();
         Swal.fire({
           icon: 'success',
-          title: 'registrarEstudiante ...',
-          text: 'Se registró exitosamente los datos del estudiante!',
+          title: 'actualizarEstudiante ...',
+          text: '¡Se actualizó exitosamente el estudiante!',
         });
         this.estudianteForm.reset();
         this.getEstudiantes();
+        this.isEdited = false;
       },
       (err: any) => {
-        console.log(this.estudianteForm.value);
-        console.log(err);
         Swal.close();
         Swal.fire({
           icon: 'error',
           title: 'Advertencia ...',
-          text: 'Ah ocurrido un error!',
+          text: '¡Ha ocurrido un error al actualizar el estudiante!',
         });
       }
     );
   }
+
+  eliminarEstudiante(estudiante:Estudiante):void{
+    Swal.fire({
+      title: 'Esta seguro de eliminar la persona seleccionada?',
+      showCancelButton:true,
+      cancelButtonText:'No',
+      confirmButtonText:'Si',
+      focusCancel:true,
+    }).then((result)=>{
+      if(result.isConfirmed){
+        this.estudianteService.eliminarEstudiante(estudiante.id_estudiante).subscribe(
+          (reult:any)=>{
+            console.log(estudiante);
+            Swal.close();
+            Swal.fire({
+              icon:'success',
+              title: 'eliminarEstudiante ...',
+              text: 'Se elimino exitosamente al estudiante!',
+            });
+            this.getEstudiantes();
+          },
+          (err:any)=>{
+            console.log(estudiante);
+            console.log(err);
+            Swal.close();
+            Swal.fire({
+              icon:'error',
+              title: 'Advertencia ...',
+              text: 'Ah ocurrido un error al eliminar Estudiante!',
+            });
+          }
+        );
+
+      }//end if
+    })//end then
+  }//end metodo
+
+
+  
+
+
+
 
 }
--- src/app/service/estudiante.service.ts
+++ src/app/service/estudiante.service.ts
@@ -2,28 +2,34 @@ import { Injectable } from '@angular/core';
 import { Estudiante } from '../model/estudiante';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { getConexionBackend } from '../constants';
 @Injectable({
   providedIn: 'root'
 })
 export class EstudianteService {
-  readonly BASE_URL: string='http://localhost:5000/';
+  readonly BASE_URL: string | undefined;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    this.BASE_URL=getConexionBackend();
+    this.BASE_URL=`${this.BASE_URL}/estudiante_routes/`
+    console.log(this.BASE_URL)
+  }
 
   getEstudiantes(): Observable <Estudiante[]>{
-      return this.http.get<Estudiante[]>(`${this.BASE_URL}/get_estudiantes`);
+      return this.http.get<Estudiante[]>(`${this.BASE_URL}get_estudiantes`);
   }
 
   registrarEstudiante(form: any){
-      return this.http.post(`${this.BASE_URL}/create_estudiante`, form);
+      return this.http.post(`${this.BASE_URL}create_estudiante`, form);
   }
 
-  actualizarEstudiante (form: any){
-      return this.http.post(`${this.BASE_URL}/update_estudiante`, form);
+  actualizarEstudiante (id: number, form: any){
+      return this.http.put(`${this.BASE_URL}update_estudiante/${id}`, form);
   }
 
-  eliminarEstudiante (estudiante: Estudiante){
-      return this.http.delete(`${this.BASE_URL}/delete`, {body:estudiante});
+  eliminarEstudiante (id: number){
+      return this.http.delete(`${this.BASE_URL}delete_estudiante/${id}`);
   }
+
 
 }
